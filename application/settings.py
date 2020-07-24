b99cba4054feb605810c174f154ba3c831f3dbeb
Rajeet <mangratirajit@gmail.com>
2020-07-24 23:14:27 +0545

    ok

--- application/settings.py
+++ application/settings.py
@@ -136,3 +136,11 @@ AUTHENTICATION_BACKENDS = (
 )
 
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'  
+MAILER_EMAIL_BACKEND = EMAIL_BACKEND  
+EMAIL_HOST = 'smtp.gmail.com'  
+EMAIL_HOST_PASSWORD = '**********'  
+EMAIL_HOST_USER = 'test@gmail.com'  
+EMAIL_PORT = 465  
+EMAIL_USE_SSL = True  
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
